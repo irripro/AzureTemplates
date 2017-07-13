8e1f4df0a6ac06051a531065feb9be839ecd8fd7
alihhussain <alhussai@microsoft.com>
2017-07-13 12:41:41 -0700

    ASR Stuff

--- Automation Demo/VMCustomData/Step3/asrdeploy.py
+++ Automation Demo/VMCustomData/Step3/asrdeploy.py
@@ -1,6 +1,8 @@
 import winrm
 
-ps_script = """wget http://aka.ms/unifiedinstaller_eus -OutFile "C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe" """
+ps_script = """wget http://aka.ms/unifiedinstaller_eus -OutFile "C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe" 
+C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe /q /x:C:\WindowsAzure\Extracted
+"""
 s = winrm.Session('{asrpublicip}', auth=('alihhussain', 'asrdemo@teamcim123'))
 
 r = s.run_ps(ps_script)
