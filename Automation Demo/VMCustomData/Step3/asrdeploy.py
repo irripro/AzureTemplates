2839337ec39cc5e6537d7e92e1ede75b990e5771
alihhussain <alhussai@microsoft.com>
2017-07-15 00:43:41 -0700

    ASR Stuff

--- Automation Demo/VMCustomData/Step3/asrdeploy.py
+++ Automation Demo/VMCustomData/Step3/asrdeploy.py
@@ -4,7 +4,9 @@ ps_script = """wget http://aka.ms/unifiedinstaller_eus -OutFile "C:\WindowsAzure
 C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe /q /x:C:\WindowsAzure\Extracted
 """
 s = winrm.Session('{asrpublicip}', auth=('alihhussain', 'asrdemo@teamcim123'))
-
 r = s.run_ps(ps_script)
 
-#print("The standard error is: %s" %r.std_err)
+ps_scripttwo = """Get-Module PowerShellGet -list | Select-Object Name,Version,Path
+"""
+r = s.run_ps(ps_scripttwo)
+print("The standard error is: %s" %r.std_out)
