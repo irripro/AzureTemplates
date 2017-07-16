13da7d391b702ff406c003a0162e9d9e4b697ad2
alihhussain <alhussai@microsoft.com>
2017-07-15 22:14:24 -0700

    ASR Stuff

--- Automation Demo/VMCustomData/Step3/asrdeploy.py
+++ Automation Demo/VMCustomData/Step3/asrdeploy.py
@@ -20,8 +20,6 @@ $SecurePassword = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $Password
 $tenant = "{tenant}"
 Login-AzureRmAccount -Credential $Cred -ServicePrincipal -TenantId $tenant
-
-#Get the vault creds
 $Vault01 = Get-AzureRmRecoveryServicesVault -Name "asrvault"
 $CredsPath = "C:\WindowsAzure\"
 $Credsfilename = Get-AzureRmRecoveryServicesVaultSettingsFile -Backup -Vault $Vault01 -Path $CredsPath
