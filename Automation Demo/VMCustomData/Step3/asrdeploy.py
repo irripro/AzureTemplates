cafc3d0c62755e92b90458d52daa2e27ec7ad3b8
alihhussain <alhussai@microsoft.com>
2017-07-15 19:00:55 -0700

    ASR Stuff

--- Automation Demo/VMCustomData/Step3/asrdeploy.py
+++ Automation Demo/VMCustomData/Step3/asrdeploy.py
@@ -13,3 +13,18 @@ Import-Module AzureRM
 """
 r = s.run_ps(ps_scripttwo)
 print("The standard error is: %s" %r.std_out)
+
+ps_scriptthree = """
+#Login to Azure Subscription
+$UserName = "{appid}"
+$Password = "{passwd}"
+$SecurePassword = ConvertTo-SecureString -AsPlainText $Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecurePassword
+$tenant = "{tenant}"
+Login-AzureRmAccount -Credential $Cred -ServicePrincipal -TenantId $tenant
+
+#Get the vault creds
+$Vault01 = Get-AzureRmRecoveryServicesVault -Name "asrvault"
+$CredsPath = "C:\WindowsAzure\"
+$Credsfilename = Get-AzureRmRecoveryServicesVaultSettingsFile -Backup -Vault $Vault01 -Path $CredsPath
+"""
