c9c17c6ff188948aab571b074d15ded31f56230f
alihhussain <alhussai@microsoft.com>
2017-07-16 16:40:16 -0700

    ASR Stuff

--- Automation Demo/VMCustomData/Step3/asrdeploy.py
+++ Automation Demo/VMCustomData/Step3/asrdeploy.py
@@ -1,21 +1,27 @@
 import winrm
-#C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe /q /x:C:\WindowsAzure\Extracted
-ps_script = """wget http://aka.ms/unifiedinstaller_eus -OutFile "C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe" 
-"""
-s = winrm.Session('{asrpublicip}', auth=('alihhussain', 'asrdemo@teamcim123'))
-r = s.run_ps(ps_script)
 
-ps_scripttwo = """Get-Module PowerShellGet -list | Select-Object Name,Version,Path
-Install-PackageProvider -Name NuGet -Force
-Install-Module AzureRM -Force
-Import-Module AzureRM
-"""
-r = s.run_ps(ps_scripttwo)
-print("The standard output is: %s" %r.std_out)
+def getasrinstaller():
+    #C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe /q /x:C:\WindowsAzure\Extracted
+    ps_script = """wget http://aka.ms/unifiedinstaller_eus -OutFile "C:\WindowsAzure\MicrosoftAzureSiteRecoveryUnifiedSetup.exe" 
+    """
+    s = winrm.Session('{asrpublicip}', auth=('alihhussain', 'asrdemo@teamcim123'))
+    r = s.run_ps(ps_script)
 
-ps_scriptthree = """wget https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/Automation%20Demo/VMCustomData/Step3/asr_cred_dl.ps1 -OutFile "C:\WindowsAzure\asr_cred_dl.ps1"
-"""
+def setupnewpowershell():
+    ps_scripttwo = """Get-Module PowerShellGet -list | Select-Object Name,Version,Path
+    Install-PackageProvider -Name NuGet -Force
+    Install-Module AzureRM -Force
+    Import-Module AzureRM
+    """
+    r = s.run_ps(ps_scripttwo)
+    print("The standard output is: %s" %r.std_out)
 
-r = s.run_ps(ps_scriptthree)
-print("The standard output is: %s" %r.std_out)
-print("The standard error is: %s" %r.std_err)
+def get_asr_cred():
+    ps_scriptthree = """
+    """
+    r = s.run_ps(ps_scriptthree)
+    print("The standard output is: %s" %r.std_out)
+    print("The standard error is: %s" %r.std_err)
+
+getasrinstaller()
+setupnewpowershell()
