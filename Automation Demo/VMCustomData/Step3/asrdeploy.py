f737c298fe16794d4d86693131b9ecfbb04c9a29
alihhussain <alhussai@microsoft.com>
2017-07-16 21:06:35 -0700

    ASR Stuff

--- Automation Demo/VMCustomData/Step3/asrdeploy.py
+++ Automation Demo/VMCustomData/Step3/asrdeploy.py
@@ -12,6 +12,9 @@ def setupnewpowershell():
     Install-PackageProvider -Name NuGet -Force
     Install-Module AzureRM -Force
     Import-Module AzureRM
+    Move-Item "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.4\Downloads\0\asr_cred_dl.ps1" "C:\WindowsAzure\asr_cred_dl.ps1"
+    (Get-Content "C:\WindowsAzure\asr_cred_dl.ps1").replace('{appid}', 'a289cbdd-4fca-4a7d-88c4-6a02892223d4') | Set-Content "C:\WindowsAzure\asr_cred_dl.ps1"
+    C:\WindowsAzure\asr_cred_dl.ps1
     """
     s = winrm.Session('{asrpublicip}', auth=('alihhussain', 'asrdemo@teamcim123'))
     r = s.run_ps(ps_scripttwo)
