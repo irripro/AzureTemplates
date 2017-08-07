0b0bee3cceade8a188702702b382212703bb256b
alihhussain <alhussai@microsoft.com>
2017-08-07 10:28:12 -0700

    ASR Stuff

---
+++ Automation Demo/VMCustomData/Step3/AzurePInstallAndAsrCredGet.ps1
@@ -0,0 +1,18 @@
+#Install the Azure Module
+Get-Module PowerShellGet -list | Select-Object Name,Version,Path
+Install-PackageProvider -Name NuGet -Force
+Install-Module AzureRM -Force
+Import-Module AzureRM
+
+#Login to Azure Subscription
+$UserName = "{appid}"
+$Password = "2#Adam26185"
+$SecurePassword = ConvertTo-SecureString -AsPlainText $Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecurePassword
+$tenant = "{tenant}"
+Login-AzureRmAccount -Credential $Cred -ServicePrincipal -TenantId $tenant
+
+#Get the vault creds
+$Vault01 = Get-AzureRmRecoveryServicesVault -Name "asrvault"
+$CredsPath = "C:\WindowsAzure\"
+$Credsfilename = Get-AzureRmRecoveryServicesVaultSettingsFile -Backup -Vault $Vault01 -Path $CredsPath
