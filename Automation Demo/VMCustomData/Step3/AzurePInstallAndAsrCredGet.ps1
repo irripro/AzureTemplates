f802d4664f04f4d42ff88f21a8d4a0e4cb825e88
alihhussain <alhussai@microsoft.com>
2017-08-10 08:09:45 -0700

    ASR Stuff

--- Automation Demo/VMCustomData/Step3/AzurePInstallAndAsrCredGet.ps1
+++ Automation Demo/VMCustomData/Step3/AzurePInstallAndAsrCredGet.ps1
@@ -28,4 +28,4 @@ mkdir "C:\ASR\Installation"
 cd "C:\ASR\Installation"
 New-Item -ItemType file "C:\ASR\Installation\passphrase.txt"
 $asrcredfilename = Get-ChildItem "C:\ASR\asrcredfolder\" -name
-C:\ASR\UNIFIEDSETUP.EXE /AcceptThirdpartyEULA /servermode "CS" /InstallLocation "C:\ASR\Installation" /MySQLCredsFilePath "C:\ASR\sql.cred" /VaultCredsFilePath "C:\ASR\asrcredfolder\$asrcredfilename" /EnvType "NonVMware" /SkipSpaceCheck /PSIP "{public_ip}" /CSIP "{public_ip}" /PassphraseFilePath "C:\ASR\Installation\passphrase.txt"
+#C:\ASR\UNIFIEDSETUP.EXE /AcceptThirdpartyEULA /servermode "CS" /InstallLocation "C:\ASR\Installation" /MySQLCredsFilePath "C:\ASR\sql.cred" /VaultCredsFilePath "C:\ASR\asrcredfolder\$asrcredfilename" /EnvType "NonVMware" /SkipSpaceCheck /PSIP "{public_ip}" /CSIP "{public_ip}" /PassphraseFilePath "C:\ASR\Installation\passphrase.txt"
