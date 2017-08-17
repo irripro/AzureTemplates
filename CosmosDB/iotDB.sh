401ba8ceca54752d2b94aebc481741451c7f83e4
alihhussain <alhussai@microsoft.com>
2017-08-17 15:50:43 -0700

    IoT

--- CosmosDB/iotDB.sh
+++ CosmosDB/iotDB.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Define the Variables
-dblocation = "eastus"
-dbresourcegroup = "iotdbrg"
-dbname = "iotdb"
-dbkind ="MongoDB"
+dblocation="eastus"
+dbresourcegroup="iotdbrg"
+dbname="iotdb"
+dbkind="MongoDB"
 
 #Craete the Resource Group
 az group create -l $dblocation -n $dbresourcegroup
@@ -12,4 +12,3 @@ az group create -l $dblocation -n $dbresourcegroup
 az cosmosdb create -n $dbname -g $dbresourcegroup --kind $dbkind
 az cosmosdb show -n $dbname -g IoT | jq .documentEndpoint | awk -F '"' '{print $2}' > /tmp/iotdb_endpoint.txt
 az cosmosdb list-keys -n $dbname -g $dbresourcegroup | jq .primaryMasterKey | awk -F '"' '{print $2}' > /tmp/iotdbkey.txt
-
