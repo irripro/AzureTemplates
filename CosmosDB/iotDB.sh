0bbe28060ff8ddaa04c2f8ab9f39186ab48a9000
alihhussain <alhussai@microsoft.com>
2017-08-17 16:29:37 -0700

    IoT

--- CosmosDB/iotDB.sh
+++ CosmosDB/iotDB.sh
@@ -12,3 +12,7 @@ az group create -l $dblocation -n $dbresourcegroup
 az cosmosdb create -n $dbname -g $dbresourcegroup --kind $dbkind
 az cosmosdb show -n $dbname -g $dbresourcegroup | jq .documentEndpoint | awk -F '"' '{print $2}' > iotdb_endpoint.txt
 az cosmosdb list-keys -n $dbname -g $dbresourcegroup | jq .primaryMasterKey | awk -F '"' '{print $2}' > iotdbkey.txt
+
+az cosmosdb collection create --collection-name mongodbcollection --db-name $dbname
+
+az cosmosdb collection list --db-name $dbname --key $(cat iotdbkey.txt) -g $dbresourcegroup --url-connection $(cat iotdb_endpoint.txt) --name $dbname
