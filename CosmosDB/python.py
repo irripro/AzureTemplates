ac416d268cef55b690e3f0415672f69376876ac5
alihhussain <alhussai@microsoft.com>
2017-08-17 19:39:02 -0700

    IoT

--- CosmosDB/python.py
+++ CosmosDB/python.py
@@ -1,8 +1,20 @@
 import pymongo
 import os
 
+#Define the connection
 url = "{pythonmongodburl}"
 client = pymongo.MongoClient(uri)
+
 db = client.iotdb.iotdbcollection
 student_record = "jsonobject"
+
 db.insert(student_record)
+
+results = db.find()
+
+#Loop through the results 
+for record in results:
+    print(record)
+
+# close the connection to MongoDB
+connection.close()
