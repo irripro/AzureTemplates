2963c1d05d7120be4ff8f43714647da65418412e
alihhussain <alhussai@microsoft.com>
2017-08-17 19:41:43 -0700

    IoT

--- CosmosDB/iotDB.sh
+++ CosmosDB/iotDB.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Define the Variables
 dblocation="eastus"
-dbresourcegroup="iotdbrgsecond"
+dbresourcegroup="iotdbrg"
 dbname="iotdb"
 dbkind="MongoDB"
-dbaccountname="iotdbaccount25"
+dbaccountname="iotdbaccount"
 dbcollection="iotdbcollection"
 
 samplejsonobject="{'joystick': {}, 'environmental': {'pressure': {'unit': 'mbar', 'value': 1012.033935546875}, 'temperature': {'unit': 'C', 'value': 32.19939422607422}, 'humidity': {'unit': '%RH', 'value': 41.28971862792969}}, 'inertial': {'orientation': {'yaw': 130.16238225817773, 'pitch': 0.5666904434304971, 'roll': 2.1385155805448903, 'unit': 'degrees', 'compass': 130.1553061821976}, 'accelerometer': {'y': 0.0369044728577137, 'x': -0.007158233784139156, 'z': 1.02303946018219, 'unit': 'g'}}, 'host': 'iot7643', 'name': 'sense-hat'}"
