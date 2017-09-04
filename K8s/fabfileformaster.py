d68a8fb983df016e7f97f679332e7c13020d7a5b
alihhussain <alhussai@microsoft.com>
2017-09-04 16:40:56 -0700

    K8S

--- K8s/fabfileformaster.py
+++ K8s/fabfileformaster.py
@@ -4,28 +4,9 @@ from fabric.api import *
 from pprint import pprint
 
 vm0 = "{elbpublicdns}:8022"
-vm1 = "{elbpublicdns}:8023"
-vm2 = "{elbpublicdns}:8024"
-vm3 = "{elbpublicdns}:8025"
-vm4 = "{elbpublicdns}:8026"
-vm5 = "{elbpublicdns}:8027"
-vm6 = "{elbpublicdns}:8028"
 
 totalnodes = {nodeschosen}   
-if totalnodes == 2:
-    env.hosts = [vm0,vm1]
-elif totalnodes == 3:
-    env.hosts = [vm0,vm1,vm2]
-elif totalnodes == 4:
-    env.hosts = [vm0,vm1,vm2,vm3]
-elif totalnodes == 5:
-    env.hosts = [vm0,vm1,vm2,vm3,vm4]
-elif totalnodes == 6:
-    env.hosts = [vm0,vm1,vm2,vm3,vm4,vm5]
-elif totalnodes == 7:
-    env.hosts = [vm0,vm1,vm2,vm3,vm4,vm5,vm6]
-else:
-    sys.exit("Select the right amount of nodes")    
+env.hosts = [vm0]
         
 def deployall():
     env.user = 'azureuser'
@@ -35,20 +16,7 @@ def deployall():
     env.output_prefix = False
     env.colorize_errors = True
     env.linewise = True
-    with hide('everything'):
-        sudo("yum upgrade -y")
-        sudo("yum update -y")
-    sudo("yum install -y ntp")
-    sudo("systemctl enable ntpd && systemctl start ntpd")
-    sudo("systemctl status ntpd")
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/hosts' -o /etc/hosts""")
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/virt7-docker-common-release.repo' -o /etc/yum.repos.d/virt7-docker-common-release.repo""")
-    sudo("yum update -y")
-    sudo("groupadd docker")
-    sudo("yum -y install --enablerepo=virt7-docker-common-release docker kubernetes etcd flannel")
-    sudo("systemctl start docker")
-    sudo("systemctl enable docker")
-    sudo("usermod -aG docker %s" %env.user)
+    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_config' -o /etc/kubernetes/config""")
     try:
         sudo("init 6")
     except:
