cac9f76f66fbe33486cb711c6f366fc1eb2688ee
alihhussain <alhussai@microsoft.com>
2017-09-04 23:07:39 -0700

    K8S

--- K8s/fabfilefornodes.py
+++ K8s/fabfilefornodes.py
@@ -3,7 +3,6 @@ import sys, json
 from fabric.api import *
 from pprint import pprint
 
-vm0 = "{elbpublicdns}:8022"
 vm1 = "{elbpublicdns}:8023"
 vm2 = "{elbpublicdns}:8024"
 vm3 = "{elbpublicdns}:8025"
@@ -11,19 +10,21 @@ vm4 = "{elbpublicdns}:8026"
 vm5 = "{elbpublicdns}:8027"
 vm6 = "{elbpublicdns}:8028"
 
+vm0PiP = "VM0"
+
 totalnodes = {nodeschosen}   
 if totalnodes == 2:
-    env.hosts = [vm0,vm1]
+    env.hosts = [vm1]
 elif totalnodes == 3:
-    env.hosts = [vm0,vm1,vm2]
+    env.hosts = [vm1,vm2]
 elif totalnodes == 4:
-    env.hosts = [vm0,vm1,vm2,vm3]
+    env.hosts = [vm1,vm2,vm3]
 elif totalnodes == 5:
-    env.hosts = [vm0,vm1,vm2,vm3,vm4]
+    env.hosts = [vm1,vm2,vm3,vm4]
 elif totalnodes == 6:
-    env.hosts = [vm0,vm1,vm2,vm3,vm4,vm5]
+    env.hosts = [vm1,vm2,vm3,vm4,vm5]
 elif totalnodes == 7:
-    env.hosts = [vm0,vm1,vm2,vm3,vm4,vm5,vm6]
+    env.hosts = [vm1,vm2,vm3,vm4,vm5,vm6]
 else:
     sys.exit("Select the right amount of nodes")    
         
@@ -35,20 +36,11 @@ def deployall():
     env.output_prefix = False
     env.colorize_errors = True
     env.linewise = True
-    with hide('everything'):
-        sudo("yum upgrade -y")
-        sudo("yum update -y")
-    sudo("yum install -y ntp")
-    sudo("systemctl enable ntpd && systemctl start ntpd")
-    sudo("systemctl status ntpd")
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/hosts' -o /etc/hosts""")
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/virt7-docker-common-release.repo' -o /etc/yum.repos.d/virt7-docker-common-release.repo""")
-    sudo("yum update -y")
-    sudo("groupadd docker")
-    sudo("yum -y install --enablerepo=virt7-docker-common-release docker kubernetes etcd flannel")
-    sudo("systemctl start docker")
-    sudo("systemctl enable docker")
-    sudo("usermod -aG docker %s" %env.user)
+    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_config' -o /etc/kubernetes/config""")
+    sudo("""sed -i "s#{masterprivateip}#%s#g" /etc/kubernetes/config""" %vm0PiP)
+    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_kubelet' -o /etc/kubernetes/kubelet""")
+    sudo("""sed -i "s#{masterprivateip}#%s#g" /etc/kubernetes/kubelet""" %vm0PiP)
+    sudo("""sed -i "s#{nodename}#$(hostname)#g" /etc/kubernetes/kubelet""")
     try:
         sudo("init 6")
     except:
