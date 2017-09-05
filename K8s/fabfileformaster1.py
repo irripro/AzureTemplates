a8bd066ae0f85274cb49b95f61208a18a6990b85
alihhussain <alhussai@microsoft.com>
2017-09-04 21:56:36 -0700

    K8S

--- K8s/fabfileformaster1.py
+++ K8s/fabfileformaster1.py
@@ -4,7 +4,7 @@ from fabric.api import *
 from pprint import pprint
 
 vm0 = "{elbpublicdns}:8022"
-vm0PiP = "10.0.0.4"
+vm0PiP = "VM0"
 totalnodes = {nodeschosen}   
 env.hosts = [vm0]
         
@@ -16,7 +16,14 @@ def deployall():
     env.output_prefix = False
     env.colorize_errors = True
     env.linewise = True
+    sudo("yum -y install --enablerepo=virt7-docker-common-release etcd")
     sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_config' -o /etc/kubernetes/config""")
+    sudo("""sed -i "s#{masterprivateip}#%s#g" /etc/kubernetes/config""" %vm0PiP)
+    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_etcd_etcd.conf' -o /etc/etcd/etcd.conf""")
+    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_apiserver' -o /etc/kubernetes/apiserver""")
+    sudo("systemctl enable etcd kube-apiserver kube-controller-manager kube-scheduler")
+    sudo("systemctl start etcd kube-apiserver kube-controller-manager kube-scheduler")
+    sudo("systemctl status etcd kube-apiserver kube-controller-manager kube-scheduler | grep (running)")
 
     try:
         sudo("init 6")
