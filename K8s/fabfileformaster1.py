72bac763c0c9575b883255a7e94375e1bca509dc
alihhussain <alhussai@microsoft.com>
2017-09-04 21:10:25 -0700

    K8S

--- K8s/fabfileformaster1.py
+++ K8s/fabfileformaster1.py
@@ -17,25 +17,6 @@ def deployall():
     env.colorize_errors = True
     env.linewise = True
     sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_config' -o /etc/kubernetes/config""")
-    sudo("""sed -i "s#{masterprivateip}#%s#g" /etc/kubernetes/config""" %vm0PiP)
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_etcd_etcd.conf' -o /etc/etcd/etcd.conf""")
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/make-ca-cert.sh' -o /tmp/make-ca-cert.sh""")
-    sudo("""/bin/bash /tmp/make-ca-cert.sh '%s' 'IP:%s,IP:10.254.0.1,DNS:kubernetes,DNS:kubernetes.default,DNS:kubernetes.default.svc,DNS:kubernetes.default.svc.cluster.local'""" %(vm0PiP,vm0PiP))
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_apiserver' -o /etc/kubernetes/apiserver""")
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_kubernetes_controller-manager' -o /etc/kubernetes/controller-manager""")
-    sudo("systemctl start etcd")
-    sudo("etcdctl mkdir /kube-centos/network")
-    sudo("""etcdctl mk /kube-centos/network/config '{ \"Network\": \"172.30.0.0/16\", \"SubnetLen\": 24, \"Backend\": { \"Type\": \"vxlan\" } }'""")
-    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/etc_sysconfig_flanneld' -o /etc/sysconfig/flanneld""")
-    sudo("""sed -i "s#{masterprivateip}#%s#g" /etc/sysconfig/flanneld""" %vm0PiP)
-    sudo("systemctl enable kube-apiserver")
-    sudo("systemctl start kube-apiserver")
-#    sudo("systemctl enable kube-controller-manager")
-#    sudo("systemctl start kube-controller-manager")
-#    sudo("systemctl enable kube-scheduler")
-#    sudo("systemctl start kube-scheduler")
-#    sudo("systemctl enable flanneld")
-#    sudo("systemctl start flanneld")
 
     try:
         sudo("init 6")
