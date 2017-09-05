da52f56d668c4b41aee4de59280f2e248891400a
alihhussain <alhussai@microsoft.com>
2017-09-04 21:55:05 -0700

    K8S

--- K8s/fabfileforall1.py
+++ K8s/fabfileforall1.py
@@ -45,9 +45,9 @@ def deployall():
     sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/virt7-docker-common-release.repo' -o /etc/yum.repos.d/virt7-docker-common-release.repo""")
     sudo("yum update -y")
     sudo("groupadd docker")
-    sudo("yum -y install --enablerepo=virt7-docker-common-release kubernetes docker")
-    sudo("systemctl start docker")
+    sudo("yum -y install --enablerepo=virt7-docker-common-release kubernetes docker etcd")
     sudo("systemctl enable docker")
+    sudo("systemctl start docker")
     sudo("usermod -aG docker %s" %env.user)
     try:
         sudo("init 6")
