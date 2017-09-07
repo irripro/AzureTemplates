a1092bf10bd6d78f110cb7059dc51db6c0060a47
alihhussain <alhussai@microsoft.com>
2017-09-07 10:03:53 -0700

    ASR Stuff

--- K8s/kubeadm/fabfileforall.py
+++ K8s/kubeadm/fabfileforall.py
@@ -51,16 +51,25 @@ def deployall():
     sudo("systemctl enable docker")
     sudo("systemctl start docker")
     sudo("usermod -aG docker %s" %env.user)
-#    sudo("""curl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl -o /usr/bin/kubectl""")
-#    sudo("chmod +x /usr/bin/kubectl")
-#    run("""echo "source <(kubectl completion bash)" >> ~/.bashrc""")
-#    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/kubeadm/etc_yum.repos.d_kubernetes.repo' -o /etc/yum.repos.d/kubernetes.repo""")
-#    sudo("yum update -y")
-#    sudo("setenforce 0")
-#    sudo("yum install -y kubelet kubeadm")
-#    sudo("systemctl enable kubelet && systemctl start kubelet")
-#    sudo("echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables")
+    sudo("""curl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl""")
+    sudo("chmod +x /usr/local/bin/kubectl")
+    run("""echo "source <(kubectl completion bash)" >> ~/.bashrc""")
+    sudo("""curl 'https://raw.githubusercontent.com/alihhussain/AzureTemplates/master/K8s/kubeadm/etc_yum.repos.d_kubernetes.repo' -o /etc/yum.repos.d/kubernetes.repo""")
+    sudo("yum update -y")
+    sudo("setenforce 0")
+    sudo("yum install -y kubelet kubeadm")
+    sudo("echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables")
     try:
         sudo("init 6")
     except:
         print("This was fine just restarted the system.")
+        
+def deployallpart2():
+    env.user = 'azureuser'
+    env.key_filename = '/var/lib/jenkins/.ssh/id_rsa'
+    #env.combine_stderr = False
+    env.disable_known_hosts = True
+    env.output_prefix = False
+    env.colorize_errors = True
+    env.linewise = True
+    sudo("systemctl enable kubelet && systemctl start kubelet")
