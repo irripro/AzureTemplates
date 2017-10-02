4e1aba97b2eed6754480cc36af52c0bc1f960c64
alihhussain <alhussai@microsoft.com>
2017-10-02 16:56:55 -0700

    ASR Stuff

--- pi/Relay/main.py
+++ pi/Relay/main.py
@@ -18,17 +18,14 @@ title2 =         app.config['TITLE2']
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        # Return index 
-        return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
+        return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2, message="You have just started")
 
     elif request.method == 'POST':
 
-        if request.form['vote'] == 'reset':
-            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
-        elif request.form['vote'] == button1:
-            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
-        elif request.form['vote'] == button2:
-            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
+        if request.form['red'] == button1:
+            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2, message="You have chosen red ON.")
+        elif request.form['red'] == button2:
+            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2, message="You have chosen red OFF.")
             
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True, port=5000)
