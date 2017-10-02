da727f7716d0c64eabdbf9b67025242776d5b3dc
alihhussain <alhussai@microsoft.com>
2017-10-02 16:43:09 -0700

    ASR Stuff

--- pi/Relay/main.py
+++ pi/Relay/main.py
@@ -19,16 +19,16 @@ title2 =         app.config['TITLE2']
 def index():
     if request.method == 'GET':
         # Return index 
-        return render_template("index.html", button1=button1, button2=button2, button3=button3, button4=button4, title=title, title1=title1,title2=title2)
+        return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
 
     elif request.method == 'POST':
 
         if request.form['vote'] == 'reset':
-            return render_template("index.html", button1=button1, button2=button2, button3=button3, button4=button4, title=title, title1=title1,title2=title2)
+            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
         elif request.form['vote'] == button1:
-            return render_template("index.html", button1=button1, button2=button2, button3=button3, button4=button4, title=title, title1=title1,title2=title2)
+            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
         elif request.form['vote'] == button2:
-            return render_template("index.html", button1=button1, button2=button2, button3=button3, button4=button4, title=title, title1=title1,title2=title2)
+            return render_template("index.html", button1=button1, button2=button2, title=title, title1=title1,title2=title2)
             
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True, port=5000)
