aa2d747aede28cd8753c580f28db034fea47c7c7
alihhussain <alhussai@microsoft.com>
2017-10-04 04:20:01 -0700

    ASR Stuff

--- pi/Relay/main.py
+++ pi/Relay/main.py
@@ -22,8 +22,10 @@ def index():
 
     elif request.method == 'POST':
         if request.form['red'] == button1:
-            return render_template("index.html", button1=button1, button2=button2, button1value=button1value, button2value=button2value, title=title)
-        elif request.form['yellow'] == button2:
+            if button1value == "OFF":
+                button1value = "ON"
+            elif butt1value == "ON":
+                butt1value = "OFF"
             return render_template("index.html", button1=button1, button2=button2, button1value=button1value, button2value=button2value, title=title)
 
 if __name__ == "__main__":
