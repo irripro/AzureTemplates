710f9dfa124c4c364c8fbad1c1a26d91972e66c5
alihhussain <alhussai@microsoft.com>
2017-10-04 04:24:37 -0700

    ASR Stuff

--- pi/Relay/main.py
+++ pi/Relay/main.py
@@ -14,12 +14,11 @@ button1 =       app.config['VOTE1VALUE']
 button2 =       app.config['VOTE2VALUE']
 title =         app.config['TITLE']
 
-button1value = "OFF"
-button2value = "OFF"
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
+        button1value = "OFF"
+        button2value = "OFF"
         return render_template("index.html", button1=button1, button2=button2, button1value=button1value, button2value=button2value, title=title)
 
     elif request.method == 'POST':
