cb6353a185a7baad8c388911d2a712327456f648
alihhussain <alhussai@microsoft.com>
2017-10-04 04:43:39 -0700

    ASR Stuff

--- pi/Relay/main.py
+++ pi/Relay/main.py
@@ -6,27 +6,27 @@ import sys
 import pymongo
 
 app = Flask(__name__)
-global button1value, button2value
 
 # Load configurations
 app.config.from_pyfile('config_file.cfg')
-button1 =       app.config['VOTE1VALUE']  
-button2 =       app.config['VOTE2VALUE']
 title =         app.config['TITLE']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        button1value = "OFF"
-        button2value = "OFF"
-        return render_template("index.html", button1=button1, button2=button2, button1value=button1value, button2value=button2value, title=title)
+        return render_template("index.html", button1value="OFF", button2value="OFF", title=title)
 
     elif request.method == 'POST':
-        print(request.form['red'])
+        print("Red Button Value is %s " %request.form['red'])
+        print("Yellow Button Value is %s" %request.form['yellow'])
         if request.form['red'] == "ON":
-            return render_template("index.html", button1=button1, button2=button2, button1value="OFF", button2value="OFF", title=title)
+            return render_template("index.html", button1value="OFF", button2value="OFF", title=title)
         elif request.form['red'] == "OFF":
-            return render_template("index.html", button1=button1, button2=button2, button1value="ON", button2value="OFF", title=title)
+            return render_template("index.html", button1value="ON", button2value="OFF", title=title)
+        if request.form['yellow'] == "ON":
+            return render_template("index.html", button2value="OFF", title=title)
+        elif request.form['yellow'] == "OFF":
+            return render_template("index.html", button2value="ON", title=title)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True, port=5000)
