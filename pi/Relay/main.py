694f10e8a0dca7fe211a1921399edca7ce330965
Ali Hussain <alhussai@microsoft.com>
2017-10-04 19:04:02 -0700

    ASR Stuff

--- pi/Relay/main.py
+++ pi/Relay/main.py
@@ -1,39 +1,39 @@
 from flask import Flask, request, render_template
-import os
-import random
-import socket
-import sys
-import pprint
-import pymongo
 
 app = Flask(__name__)
+
+global redvalue, yellowvalue
+redvalue = "OFF"
+yellowvalue = "OFF"
 
 # Load configurations
 app.config.from_pyfile('config_file.cfg')
 title =         app.config['TITLE']
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global redvalue, yellowvalue 
     if request.method == 'GET':
-        return render_template("index.html", title=title)
+        return render_template("index.html", redvalue=redvalue, yellowvalue=yellowvalue, title=title)
 
     elif request.method == 'POST':
         response = request.form.to_dict()
         try: 
             if response['red'] == "ON":
-                print("RED ON.")
+                redvalue = "OFF"                
             elif response['red'] == "OFF":
-                print("RED OFF.")
+                redvalue = "ON" 
         except:
             pass
         try:    
             if response['yellow'] == "ON":
-                print("YELLOW ON.")
+                yellowvalue = "OFF"
             elif response['yellow'] == "OFF":
-                print("YELLOW OFF.")
+                yellowvalue = "ON"
         except:
             pass
-        return render_template("index.html", title=title)
+        return render_template("index.html", redvalue=redvalue, yellowvalue=yellowvalue, title=title)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True, port=5000)
