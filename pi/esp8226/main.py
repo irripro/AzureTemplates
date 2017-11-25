ac7119623e2446c9269c52c92e97ea8caa58e6c7
alihhussain <alhussai@microsoft.com>
2017-11-24 23:24:58 -0800

    flask

--- pi/esp8226/main.py
+++ pi/esp8226/main.py
@@ -8,23 +8,6 @@ dat = machine.Pin(2)
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
 roms = ds.scan()
 
-#HTML to send to browsers
-html = """<!DOCTYPE html>
-<html>
-<head> <title>ESP8266 LED ON/OFF</title> </head>
-<center><h2>A simple webserver for turning HUZZAH Feather LED's on and off with Micropython</h2></center>
-<center><h3>(for noobs to both the ESP8266 and Micropython)</h3></center>
-<form>
-LED0: 
-<button name="LED" value="ON0" type="submit">LED ON</button>
-<button name="LED" value="OFF0" type="submit">LED OFF</button><br><br>
-LED2: 
-<button name="LED" value="ON2" type="submit">LED ON</button>
-<button name="LED" value="OFF2" type="submit">LED OFF</button>
-</form>
-</html>
-"""
-
 def temperature():
     ds.convert_temp()
     return ds.read_temp(roms[0])
@@ -33,6 +16,7 @@ def temperature():
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
+
 while True:
     response = str(temperature())
     conn, addr = s.accept()
