c4c4050121b19d7949246f0636f00f5da485588f
alihhussain <alhussai@microsoft.com>
2017-11-26 09:52:38 -0800

    flask

--- pi/esp8226/main.py
+++ pi/esp8226/main.py
@@ -10,15 +10,15 @@ roms = ds.scan()
 
 def temperature():
     ds.convert_temp()
-    return ds.read_temp(roms[0])
+    return ((ds.read_temp(roms[0]) * 1.8) + 32)
     
 #Setup Socket WebServer
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
-
 while True:
     response = str(temperature())
+    time.sleep_ms(750)
     conn, addr = s.accept()
     request = conn.recv(1024)
     conn.send(response)
