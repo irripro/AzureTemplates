2a18182339fc192ec31f7f43c9859c61e2e5ada4
Ali Hussain <alhussai@microsoft.com>
2017-11-27 19:36:57 +0000

    flask

--- docker/autocomplete/app.py
+++ docker/autocomplete/app.py
@@ -7,47 +7,8 @@ from wtforms import TextField, Form
 
 app = Flask(__name__)
 
-cities = ["Tokyo,Japak (1)",
-          "New York,USA (2)",
-          "Sao Paulo,Brazil (3)",
-          "Seoul,South Korea (4)",
-          "Mexico City,Mexico (5)",
-          "Osaka,Japan (6)",
-          "Manila,Philippines (7)",
-          "Mumbai,India (8)",
-          "Delhi,India (9)",
-          "Jakarta,Indonesia (10)",
-          "Lagos, Nigeria (11)",
-          "Kolkata,India (12)",
-          "Cairo,Egypt (13)",
-          "Los Angeles,USA (14)",
-          "Buenos Aires,Argentina (15)",
-          "Rio de Janeiro,Brazil (16)",
-          "Moscow,Russia (17)",
-          "Shanghai,China (18)",
-          "Karachi,Pakistan (19)",
-          "Paris,France (20)",
-          "Istanbul,Turkey (21)",
-          "Nagoya,Japan (22)",
-          "Beijing,China (23)",
-          "Chicago,USA (24)",
-          "London, England (25)",
-          "Shenzhen,China (26)",
-          "Essen,Germany (27)",
-          "Tehran,Iran (28)",
-          "Bogota,Colombia (29)",
-          "Lima,Peru (30)"
-          ]
-
-
 class SearchForm(Form):
     autocomp = TextField('Guess city and find out how its ranked in the world by population', id='city_autocomplete')
-
-
-@app.route('/_autocomplete', methods=['GET'])
-def autocomplete():
-    return Response(json.dumps(cities), mimetype='application/json')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -55,4 +16,4 @@ def index():
     return render_template("city.html", form=form)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=80)
+    app.run('0.0.0.0',ssl_context=('/root/autocomplete/cert1.pem', '/root/autocomplete/privkey1.pem'), port=443)
