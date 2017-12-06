a00311e366eb312c8fb886c3053a54df508d5df6
alihhussain <alhussai@microsoft.com>
2017-12-05 22:25:57 -0800

    flask

--- pi/esp8226/boot.py
+++ pi/esp8226/boot.py
@@ -3,6 +3,16 @@
 #esp.osdebug(None)
 import gc
 import webrepl
+import ntptime
+import time
+import utime
+import machine
+import ds18x20
+import onewire
+import socket
+from machine import Pin
+from umqtt.simple import MQTTClient
+
 webrepl.start()
 gc.collect()
 
@@ -16,5 +26,34 @@ def do_connect():
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
-
+   
 do_connect()
+time.sleep_ms(5000)
+ntptime.settime()
+
+
+dat = machine.Pin(5)
+ds = ds18x20.DS18X20(onewire.OneWire(dat))
+roms = ds.scan()
+relay = Pin(0,Pin.OUT)
+
+def temperature():
+    ds.convert_temp()
+    return ((ds.read_temp(roms[0]) * 1.8) + 32)
+
+def sendmsg(server="10.0.0.160"):
+    msg = str("%s:%s" %(utime.localtime(),temperature()))
+    c = MQTTClient("umqtt_client", server)
+    c.connect()
+    c.publish(b"sensor/temperature", b"{}".format(temperature())
+    c.disconnect()
+
+rtc = machine.RTC()
+rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+
+# set RTC.ALARM0 to fire after 10 seconds (waking the device)
+rtc.alarm(rtc.ALARM0, 60000)
+
+sendmsg()
+# put the device to sleep
+#machine.deepsleep()
--- pi/esp8226/main.py
+++ pi/esp8226/main.py
@@ -3,15 +3,36 @@ import machine
 import ds18x20
 import onewire
 import socket
+from machine import Pin
+from umqtt.simple import MQTTClient
 
-dat = machine.Pin(2)
+dat = machine.Pin(5)
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
 roms = ds.scan()
+relay = Pin(0,Pin.OUT)
 
 def temperature():
     ds.convert_temp()
     return ((ds.read_temp(roms[0]) * 1.8) + 32)
-    
+
+rtc = machine.RTC()
+rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+
+# set RTC.ALARM0 to fire after 10 seconds (waking the device)
+rtc.alarm(rtc.ALARM0, 60000)
+
+def sendmsg(server="10.0.0.160"):
+    msg = str("%s:%s" %(utime.localtime(),temperature()))
+    c = MQTTClient("umqtt_client", server)
+    c.connect()
+    c.publish(b"sensor/temperature", b"{}".format(temperature())
+    c.disconnect()
+
+sendmsg()
+# put the device to sleep
+machine.deepsleep()
+
+'''   
 #Setup Socket WebServer
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
@@ -22,4 +43,5 @@ while True:
     conn, addr = s.accept()
     request = conn.recv(1024)
     conn.send(response)
-    conn.close()
+    conn.close() 
+'''
