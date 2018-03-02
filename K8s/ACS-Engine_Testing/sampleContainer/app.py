d2ce6be58129711cbce687e5717bd63c600dc933
Ali Hussain <alhussai@microsoft.com>
2018-03-02 22:26:20 +0000

    sogeti

--- K8s/ACS-Engine_Testing/sampleContainer/app.py
+++ K8s/ACS-Engine_Testing/sampleContainer/app.py
@@ -3,11 +3,12 @@ from fabric.api import local
 
 app = Flask(__name__)
 
-output = str(local("""ip -o -4 a | awk '$2 == "eth0" { gsub(/\/.*/, "", $4); print $4 }'""", capture=True))
+IP_output = str(local("""ip -o -4 a | awk '$2 == "eth0" { gsub(/\/.*/, "", $4); print $4 }'""", capture=True))
+ID_output = str(local("""cat /proc/self/cgroup | head -n 1 | cut -d '/' -f3""", capture=True))
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", container_IP=output)
+    return render_template("index.html", container_IP=IP_output, container_ID=ID_output)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
