220273765e6262fd212b07498fdd8544209c1fa0
Ali Hussain <alhussai@microsoft.com>
2018-03-03 18:06:15 +0000

    sogeti

--- K8s/ACS-Engine_Testing/sampleContainer/ip.py
+++
@@ -1,4 +0,0 @@
-from fabric.api import local
-
-response = local("""ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1""")
-print(response)
--- K8s/ACS-Engine_Testing/sampleContainer/ip2.py
+++
@@ -1,4 +0,0 @@
-from fabric.api import local
-
-output = str(local("""ip -o -4 a | awk '$2 == "eth0" { gsub(/\/.*/, "", $4); print $4 }'""", capture=True))
-print(output)
