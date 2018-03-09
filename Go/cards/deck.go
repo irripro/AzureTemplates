4286b4b3437c88cfb7e941fe0698da633606a349
alihhussain <alhussai@microsoft.com>
2018-03-08 21:04:14 -0800

    Go!

--- Go/cards/deck.go
+++ Go/cards/deck.go
@@ -1,6 +1,31 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Create a new type of 'deck'
 // which is a slice of string
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Clubs", "Hearts"}
+	cardValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jacks", "Queens", "King"}
+
+	for _, value := range cardValues {
+		for _, suit := range cardSuits {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- Go/cards/main.go
+++ Go/cards/main.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Ace of Spades"
+	cards := newDeck()
+	cards.print()
 }
