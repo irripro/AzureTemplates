175bf9475b561f3ac9e30cfa93b152add82276ee
alihhussain <alhussai@microsoft.com>
2018-03-08 18:26:01 -0800

    go

---
+++ Go/cards/deck.go
@@ -0,0 +1,6 @@
+package main
+
+// Create a new type of 'deck'
+// which is a slice of string
+
+type deck []string
---
+++ Go/cards/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Ace of Spades"
+}
