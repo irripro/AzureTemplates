d61b72be65d8b3577a7e59cf995adbb2b09c2c47
root <root@MININT-alhussai.redmond.corp.microsoft.com>
2018-03-26 09:43:21 -0700

    Cloud-Init

--- cloud-init/deploy.sh
+++ cloud-init/deploy.sh
@@ -9,9 +9,11 @@ az vm create \
   --custom-data myScript.sh \
   --ssh-key-value @id_rsa.pub
 
+az group create --name myResourceGroup2 --location eastus
+
 az vm create \
   --resource-group myResourceGroup2 \
   --name centos74 \
   --image OpenLogic:CentOS:7-CI:latest \
   --custom-data cloud_init_hostname.txt \
-  --generate-ssh-keys
+  --ssh-key-value @id_rsa.pub
