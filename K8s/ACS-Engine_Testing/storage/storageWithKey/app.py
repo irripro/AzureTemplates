de83bb789d1c9a21329c1e370145c462d2cc2520
alihhussain <alhussai@microsoft.com>
2018-04-30 10:52:18 -0700

    storage

--- K8s/ACS-Engine_Testing/storage/storageWithKey/app.py
+++ K8s/ACS-Engine_Testing/storage/storageWithKey/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("k8s_index.html", k8s_key=k8s_node_name)
+    return render_template("k8s_index.html", k8s_key=storageKey)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=80)
